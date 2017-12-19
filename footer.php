b399dde8c82e5090c6cbfe51e6dfdfeef9904374
Loic Norgeot <loic.norgeot@gmail.com>
2017-12-19 17:10:34 +0100

    Updates footer

--- footer.php
+++ footer.php
@@ -1,22 +1,26 @@
 	<div id="sponsors">
-		<a target="blank" href="http://www.sorbonne-universites.fr/"><img src="/wp-content/uploads/2017/02/logo_sorbonne.png" alt="" height="17"></a>
-		<a target="blank" href="http://www.paris-sorbonne.fr/"><img src="/wp-content/uploads/2017/02/logo_paris-sorbonne.png" alt="" height="17"></a>
-		<a target="blank" href="http://www.upmc.fr/"><img src="/wp-content/uploads/2017/02/logo_upmc.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.lanouvelleuniversite.fr/"><img src="/wp-content/uploads/2017/12/logo_sorbonne.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.cnrs.fr/"><img src="/wp-content/uploads/2017/02/logo_cnrs.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.carnot-smiles.fr/"><img src="/wp-content/uploads/2017/12/logo_carnot.png" alt="" height="17"></a>
+		<!--
 		<a target="blank" href="http://www.utc.fr/"><img src="/wp-content/uploads/2017/02/logo_utc.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.insead.edu/"><img src="/wp-content/uploads/2017/02/logo_insead.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.mnhn.fr/"><img src="/wp-content/uploads/2017/02/logo_museum.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.pspbb.fr/"><img src="/wp-content/uploads/2017/02/logo_pspbb.png" alt="" height="17"></a>
-		<a target="blank" href="http://www.cnrs.fr/"><img src="/wp-content/uploads/2017/02/logo_cnrs.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.inserm.fr/"><img src="/wp-content/uploads/2017/02/logo_inserm.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.ird.fr/"><img src="/wp-content/uploads/2017/02/logo_ird.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.inria.fr/"><img src="/wp-content/uploads/2017/02/logo_inria.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.ciep.fr/"><img src="/wp-content/uploads/2017/02/logo_ciep.png" alt="" height="17"></a>
-		<a target="blank" href="http://simseo.fr"><img src="/wp-content/uploads/2017/02/logo_simseo.png" alt="" height="17"></a>
 		<a target="blank" href="http://www.instituts-carnot.eu/"><img src="/wp-content/uploads/2017/02/logo_carnot.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.sorbonne-universites.fr/"><img src="/wp-content/uploads/2017/02/logo_sorbonne.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.paris-sorbonne.fr/"><img src="/wp-content/uploads/2017/02/logo_paris-sorbonne.png" alt="" height="17"></a>
+		<a target="blank" href="http://www.upmc.fr/"><img src="/wp-content/uploads/2017/02/logo_upmc.png" alt="" height="17"></a>
+		-->
 	</div>
 
 	<script src="<?php bloginfo('template_directory');?>/js/own/setup.js"></script>
 	<script src="<?php bloginfo('template_directory');?>/js/own/main.js"></script>
+
 	<script type="text/x-mathjax-config">
   		MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
 	</script>

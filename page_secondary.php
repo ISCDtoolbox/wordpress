1ad29992dbedf354f54b8f1a24aa29a93d1d0357
Loic Norgeot <loic.norgeot@gmail.com>
2017-12-19 18:15:02 +0100

    Updates secondary page and menu structure

--- page_secondary.php
+++ page_secondary.php
@@ -8,54 +8,107 @@
  */
 ?>
 
-
 <?php get_header();?>
 
+<!-- On protège les pages avec mot de passe -->
 <?php if ( ! post_password_required( $post ) ) {?>
 
 <div id="secondaryPage">
-	<?php
-		$args = array( 'post_type' => 'page', 'post_parent' => $post->ID, 'orderby' => 'menu_order', 'order'   => 'ASC' );
-		$the_query = new WP_Query( $args );
 
-		/* Ancien menu
-		if (get_depth($post->ID)==1 && ( get_post_field('post_name',get_post())=="about" ) ) {
-			echo '<div id="secondaryMenu">';
-			echo '<li><h3><a href="'.get_permalink($post->ID).'">' . $post->post_name . '</a></h3></li>';
-			echo '<ul>';
+	<!-- Menu de gauche -->
+	<?php
+		$isFaq = ( get_post_field('post_name',get_post())=="faq" ? 1 : 0 );
+		$depth = get_depth($post->ID);
+
+		//Si la page n'est pas FAQ, on affiche le menu à gauche
+		if( $isFaq==0 ){
+
+			$argsLvl1   = array( 'post_type' => 'page', 'post_parent' => $post->ID, 'orderby' => 'menu_order', 'order'   => 'ASC' );
+			$argsLvlSup = array( 	'post_type' => 'page', 'post_parent' => wp_get_post_parent_id( $post->ID ), 'orderby' => 'menu_order', 'order'   => 'ASC' );
+
+			$args = ( $depth>1 ? $argsLvlSup : $argsLvl1 );
+			$the_query = new WP_Query( $args );
+
 			$url = get_permalink($post->ID);
+			echo '<div id="secondaryMenu">';
+
+			//Tri en fonction du niveau (pages cousines ou enfantes)
+			if($depth == 1)
+				echo '<li><h3><a href="'.$url.'">' . $post->post_name . '</a></h3></li>';
+			else
+				echo '<li><h3><a href="'. get_permalink(wp_get_post_parent_id( $post->ID )).'">'. get_post(wp_get_post_parent_id( $post->ID ))->post_name. '</a></h3></li>';
+
+			//Puis les pages enfants (donc de même niveau que la page actuelle)
+			echo '<ul>';
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
 				$slug = get_post_field( 'post_name', get_post() );
-				if(get_the_permalink() == $url){
-					echo '<li class="current" href="'
-					. $slug
-					. '"><a href="'
-					. get_the_permalink()
-					.'">' . get_the_title() . '</a></li>';
-				}
-				else{
+				if(get_the_permalink() == $url)
+					echo '<li class="current" href="' . $slug . '"><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
+				else
 					echo '<li href="' . $slug . '"><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
-				}
 			}
 			wp_reset_postdata();
 			echo '</ul>';
 			echo '</div>';
 		}
-		elseif( get_depth($post->ID)==1 ){echo '';}
-		else{
-			$args = array( 	'post_type' => 'page',
-					'post_parent' => wp_get_post_parent_id( $post->ID ),
-					'orderby' => 'menu_order',
-					'order'   => 'ASC' );
+
+		/*
+		//Si la page est de premier niveau, et n'est pas FAQ, on affiche le menu
+		if ( $depth==1 && $isFaq!=0 ) {
+
+			$args = array( 'post_type' => 'page', 'post_parent' => $post->ID, 'orderby' => 'menu_order', 'order'   => 'ASC' );
 			$the_query = new WP_Query( $args );
-			echo '<div id="secondaryMenu" class="pasabout">';
+
+			$url = get_permalink($post->ID);
+			echo '<div id="secondaryMenu">';
+
+			//On affiche d'abord le titre de la page active
+			echo '<li><h3><a href="'.$url.'">' . $post->post_name . '</a></h3></li>';
+
+			//Puis les pages enfant
+			echo '<ul>';
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$slug = get_post_field( 'post_name', get_post() );
+				//Si c'est cette page, alors on la marque comme "current"
+				if(get_the_permalink() == $url){
+					echo '<li class="current" href="'.$slug. '>"'
+					.'<a href="'. get_the_permalink().'">'
+					.get_the_title()
+					.'</a></li>';
+				}
+				//Sinon, on ne la marque pas
+				else{
+					echo '<li href="' . $slug . '">'
+					.'<a href="'.get_the_permalink().'">' . get_the_title() . '</a>'
+					.'</li>';
+				}
+			}
+			//On finit
+			wp_reset_postdata();
+			echo '</ul>';
+			echo '</div>';
+		}
+		//Si la page est FAQ, on n'affiche rien
+		elseif( get_depth($post->ID)==1 ){echo '';}
+		//Si la page est de niveau supérieur, on va chercher la query de ses enfants, et on rajoute un retour
+		else{
+
+			$args = array( 	'post_type' => 'page', 'post_parent' => wp_get_post_parent_id( $post->ID ), 'orderby' => 'menu_order', 'order'   => 'ASC' );
+			$the_query = new WP_Query( $args );
+
+			$url = get_permalink($post->ID);
+			echo '<div id="secondaryMenu">';
+
+			//On affiche le titre de la page parente
 			echo '<li><h3><a href="'
 				. get_permalink(wp_get_post_parent_id( $post->ID )).'">'
 				. get_post(wp_get_post_parent_id( $post->ID ))->post_name
 				. '</a></h3></li>';
+
+			//Puis les pages enfants (donc de même niveau que la page actuelle)
 			echo '<ul>';
-			$url = get_permalink($post->ID);
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
 				$slug = get_post_field( 'post_name', get_post() );
@@ -71,62 +124,9 @@
 			echo '</div>';
 		}
 		*/
+	?>
 
-		if (get_depth($post->ID)==1 && ( get_post_field('post_name',get_post())!="faq" ) ) {
-			echo '<div id="secondaryMenu">';
-			echo '<li><h3><a href="'.get_permalink($post->ID).'">' . $post->post_name . '</a></h3></li>';
-			echo '<ul>';
-			$url = get_permalink($post->ID);
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				$slug = get_post_field( 'post_name', get_post() );
-				if(get_the_permalink() == $url){
-					echo '<li class="current" href="'
-					. $slug
-					. '"><a href="'
-					. get_the_permalink()
-					.'">' . get_the_title() . '</a></li>';
-				}
-				else{
-					echo '<li href="' . $slug . '"><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
-				}
-			}
-			wp_reset_postdata();
-			echo '</ul>';
-			echo '</div>';
-		}
-		elseif( get_depth($post->ID)==1 ){echo '';}
-		else{
-			$args = array( 	'post_type' => 'page',
-					'post_parent' => wp_get_post_parent_id( $post->ID ),
-					'orderby' => 'menu_order',
-					'order'   => 'ASC' );
-			$the_query = new WP_Query( $args );
-			echo '<div id="secondaryMenu" class="pasabout">';
-			echo '<li><h3><a href="'
-				. get_permalink(wp_get_post_parent_id( $post->ID )).'">'
-				. get_post(wp_get_post_parent_id( $post->ID ))->post_name
-				. '</a></h3></li>';
-			echo '<ul>';
-			$url = get_permalink($post->ID);
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				$slug = get_post_field( 'post_name', get_post() );
-				if(get_the_permalink() == $url){
-					echo '<li class="current" href="' . $slug . '"><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
-				}
-				else{
-					echo '<li href="' . $slug . '"><a href="'.get_the_permalink().'">' . get_the_title() . '</a></li>';
-				}
-			}
-			wp_reset_postdata();
-			echo '</ul>';
-			echo '</div>';
-		}
-
-
-		?>
-
+	<!-- Contenu -->
 	<div id="secondaryContent">
 		<div id="mainPart">
 			<?php
@@ -136,21 +136,22 @@
 		</div>
 	</div>
 
-
+	<!-- Si la page est protégée avec un mot de passe... -->
 	<?php
 	}
 	else{
 	?>
-        <div id="secondaryContent">
-                <div id="mainPart">
-		<?php echo get_the_password_form();?>
-                </div>
-        </div>
+
+	<!-- Alors on affiche le formulaire / mot de passe -->
+  <div id="secondaryContent">
+    <div id="mainPart">
+			<?php echo get_the_password_form();?>
+    </div>
+  </div>
+
 	<?php
 	}
 	?>
-
-
 
 </div>
 

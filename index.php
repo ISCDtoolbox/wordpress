f6d54a7a0c13137ac5797a96cc62a0b100a128fa
Loic Norgeot <loic.norgeot@gmail.com>
2017-12-20 10:51:21 +0100

    Updates

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Secondary page (research, contact...) for big topics
+ * Template Name: Default template (index.php)
  *
  * @package WordPress
  * @subpackage Twenty_Fourteen
@@ -9,10 +9,10 @@
 ?>
 <?php get_header();?>
 <?php the_excerpt(); ?>
-	<?php 
+	<?php
 		$args = array( 'post_type' => 'page', 'post_parent' => $post->ID );
 		$the_query = new WP_Query( $args );
-		
+
 		/*On cree le menu secondaire*/
 		if ( $the_query->have_posts() ) {
 			echo '<div id="secondaryMenu">';
@@ -26,28 +26,28 @@
 			wp_reset_postdata();
 			echo '</ul>';
 			echo '</div>';
-		} 
-		
+		}
+
 		?>
-		
+
 <div id="secondaryContent" style="">
 
-		<?php 
+		<?php
 		/*On ajoute des div de contenu, display = none, qui seront affichés lors des clics dans le menu secondaire*/
 		if ( $the_query->have_posts() ) {
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
 				$the_content = get_the_content();
-				
+
 				$content     = wpautop( $the_content, true);
 				$slug = get_post_field( 'post_name', get_post() );
 				echo '<div style="display:none;" id="' . $slug . '"><h1>' . get_the_title() . '</h1>' . $content . '</div>';
 			}
 			wp_reset_postdata();
-		} 
+		}
 		?>
 		<div style="" id="mainPart" style="">
-			<?php 
+			<?php
 				echo '<h1>' . $post->post_title . '</h1>';
 				echo apply_filters('the_content', get_post_field('post_content', $post->ID));
 			?>
